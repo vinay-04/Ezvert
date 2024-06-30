2fb9a9441c9e93df45135f298a4e155e425aa78d
Vinay Rajan S <vinayrajan04@gmail.com>
2024-06-30 21:47:44 +0530

    hehe

--- main.py
+++ main.py
@@ -1,17 +1,43 @@
-import sys
+from sys import argv
+from os import makedirs
 import ffmpeg
+import threading
+from math import ceil
+import moviepy.editor as mp
 
 
-try:
-    args = sys.argv
-    if '-h' in args or '--help' in args or len(args) != 2:
-        print('Usage:  -f <input file>\n\t-o <output file>')
+def splitFile(file, d, format):
+        ffmpeg.input(file, ss=d*60, t=60).output(f'out/{d}.{format}').run()
+        # ffmpeg.input(args[file]).output(args[output]).run()
+
+def outFile(file, format):
+    makedirs('out', exist_ok=True)
+    duration = float(ffmpeg.probe(file)['format']['duration'])
+    if duration < 180:
+        split = 1
     else:
-        file = args.index('-f')+1
-        output = args.index('-o')+1
-        ffmpeg.input(args[file]).output(args[output]).run()
+        split = (duration/60)%60
+    threads = []
+    for d in range(0, int(ceil(split))):
+        thread = threading.Thread(target=splitFile, args=(file, d, format))
+        threads.append(thread)
+        thread.start()
+    for thread in threads:
+        thread.join()
+
+if __name__ == '__main__':
+
+    try:
+        if '-h' in argv or '--help' in argv:
+            print('Usage:  -f <input file>\n\t-o <output file>')
+        else:
+            file = argv.index('-f')+1
+            output = argv.index('-o')+1
+            format = argv[output].split('.')[-1]
+            outFile(argv[file], format)
+            files = [f'out/{d}.{format}' for d in range(0, int(ceil((float(ffmpeg.probe(argv[file])['format']['duration'])/60)%60)))]
+            mp.concatenate_videoclips([mp.VideoFileClip(file) for file in files]).write_videofile(argv[output])
 
 
-
-except:
-    sys.exit('Error: Invalid arguments\nUsage:  -f <input file>\n\t-o <output file>')
+    except:
+        exit('Error: Invalid arguments\nUsage:  -f <input file>\n\t-o <output file>')
